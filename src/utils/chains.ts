1b7d98e5e9391adf89d6908737b70c8fdb6ad169
Timur Badretdinov <accounts@destiner.io>
2024-04-02 13:09:08 +0100

    Add chains util

---
+++ src/utils/chains.ts
@@ -0,0 +1,67 @@
+import { type Chain as ChainData } from 'viem';
+import {
+  mainnet,
+  sepolia,
+  optimism,
+  optimismSepolia,
+  base,
+  baseSepolia,
+  polygon,
+  polygonAmoy,
+} from 'viem/chains';
+
+const ETHEREUM = mainnet.id;
+const SEPOLIA = sepolia.id;
+const OPTIMISM = optimism.id;
+const OPTIMISM_SEPOLIA = optimismSepolia.id;
+const BASE = base.id;
+const BASE_SEPOLIA = baseSepolia.id;
+const POLYGON = polygon.id;
+const POLYGON_AMOY = polygonAmoy.id;
+
+type Chain =
+  | typeof ETHEREUM
+  | typeof SEPOLIA
+  | typeof OPTIMISM
+  | typeof OPTIMISM_SEPOLIA
+  | typeof BASE
+  | typeof BASE_SEPOLIA
+  | typeof POLYGON
+  | typeof POLYGON_AMOY;
+
+const DEFAULT_CHAIN = ETHEREUM;
+
+const CHAINS: Chain[] = [
+  ETHEREUM,
+  SEPOLIA,
+  OPTIMISM,
+  OPTIMISM_SEPOLIA,
+  BASE,
+  BASE_SEPOLIA,
+  POLYGON,
+  POLYGON_AMOY,
+];
+
+function getChainData(chainId: Chain): ChainData {
+  switch (chainId) {
+    case ETHEREUM:
+      return mainnet;
+    case SEPOLIA:
+      return sepolia;
+    case OPTIMISM:
+      return optimism;
+    case OPTIMISM_SEPOLIA:
+      return optimismSepolia;
+    case BASE:
+      return base;
+    case BASE_SEPOLIA:
+      return baseSepolia;
+    case POLYGON:
+      return polygon;
+    case POLYGON_AMOY:
+      return polygonAmoy;
+  }
+}
+
+export { CHAINS, DEFAULT_CHAIN, getChainData };
+export type { Chain };
