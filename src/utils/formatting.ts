2d2047d3eb5177888951e8872a80cda2393a1357
Timur Badretdinov <accounts@destiner.io>
2024-04-03 13:33:20 +0100

    Add address formatter

--- src/utils/formatting.ts
+++ src/utils/formatting.ts
@@ -1,9 +1,14 @@
+import { Address } from 'viem';
+
 import { RelativeTime } from './conversion';
+
+function formatAddress(value: Address, size: number): string {
+  return `${value.slice(0, 2 + size / 2)}...${value.slice(-size / 2)}`;
+}
 
 function formatRelativeTime({ value, unit }: RelativeTime): string {
   const format = new Intl.RelativeTimeFormat('en', { numeric: 'auto' });
   return format.format(value, unit);
 }
 
-// eslint-disable-next-line import/prefer-default-export
-export { formatRelativeTime };
+export { formatAddress, formatRelativeTime };
