fcb328eda65f5776fb77a644c4cb7281e01d2faf
Timur Badretdinov <accounts@destiner.io>
2024-04-03 17:03:48 +0100

    Add IconBrand component

---
+++ src/components/__common/IconBrand.vue
@@ -0,0 +1,52 @@
+<template>
+  <svg
+    width="100%"
+    height="100%"
+    viewBox="0 0 266 266"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M133 10C65.069 10 10 65.069 10 133C10 200.931 65.069 256 133 256C200.931 256 256 200.931 256 133C256 65.069 200.931 10 133 10ZM0 133C0 59.5461 59.5461 0 133 0C206.454 0 266 59.5461 266 133C266 206.454 206.454 266 133 266C59.5461 266 0 206.454 0 133Z"
+      fill="currentColor"
+    />
+    <mask
+      id="mask0_1_3"
+      style="mask-type: alpha"
+      maskUnits="userSpaceOnUse"
+      x="10"
+      y="10"
+      width="246"
+      height="246"
+    >
+      <circle
+        cx="133"
+        cy="133"
+        r="128"
+        fill="#D9D9D9"
+      />
+    </mask>
+    <g mask="url(#mask0_1_3)">
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M297 -23L-22.9999 297L-30.7781 289.222L289.222 -30.7782L297 -23Z"
+        fill="currentColor"
+      />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M267 -53L-52.9999 267L-60.7781 259.222L259.222 -60.7782L267 -53Z"
+        fill="currentColor"
+      />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M237 -83L-82.9999 237L-90.7781 229.222L229.222 -90.7782L237 -83Z"
+        fill="currentColor"
+      />
+    </g>
+  </svg>
+</template>
