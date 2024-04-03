a3156ab78df8ffb2e97ba73f04b3f6d17553d331
Timur Badretdinov <accounts@destiner.io>
2024-04-03 14:40:51 +0100

    Add ScopeTextStrip component

---
+++ src/components/__common/ScopeTextStrip.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="view">{{ value }}</div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  value: string;
+  id?: string;
+}>();
+</script>
+
+<style scoped>
+.view {
+  width: 520px;
+  padding: var(--spacing-4);
+  border: 1px solid transparent;
+  border-radius: var(--border-radius-s);
+  background: var(--color-background-secondary);
+  color: var(--color-text-primary);
+  font-family: var(--font-mono);
+  font-size: var(--font-size-m);
+}
+</style>
