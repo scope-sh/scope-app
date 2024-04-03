f75877594ada34ee56fc58664a89027422643fb0
Timur Badretdinov <accounts@destiner.io>
2024-04-03 16:57:19 +0100

    Add TooltipShortcut component

---
+++ src/components/__common/TooltipShortcut.vue
@@ -0,0 +1,36 @@
+<template>
+  <ScopeTooltip>
+    <template #trigger>
+      <slot name="trigger" />
+    </template>
+    <template #default>
+      <div class="content">
+        <div>{{ label }}</div>
+        <ScopeShortcutGroup
+          size="small"
+          :shortcuts="shortcuts"
+        />
+      </div>
+    </template>
+  </ScopeTooltip>
+</template>
+
+<script setup lang="ts">
+import { Shortcut } from '@/utils/shortcuts';
+
+import ScopeShortcutGroup from './ScopeShortcutGroup.vue';
+import ScopeTooltip from './ScopeTooltip.vue';
+
+defineProps<{
+  label: string;
+  shortcuts: Shortcut[];
+}>();
+</script>
+
+<style scoped>
+.content {
+  display: flex;
+  gap: var(--spacing-7);
+  align-items: center;
+}
+</style>
