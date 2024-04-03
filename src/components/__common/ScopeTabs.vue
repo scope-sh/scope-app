2e5c176b69e03f91e7ef9393a5cbfde614edad24
Timur Badretdinov <accounts@destiner.io>
2024-04-03 09:02:21 +0100

    Add ScopeTabs component

---
+++ src/components/__common/ScopeTabs.vue
@@ -0,0 +1,76 @@
+<template>
+  <Tabs.Root
+    :model-value="modelValue"
+    class="root"
+    @update:model-value="handleUpdate"
+  >
+    <Tabs.List as-child>
+      <div class="list">
+        <Tabs.Trigger
+          v-for="option in options"
+          :key="option.value"
+          :value="option.value"
+          as-child
+        >
+          <button class="tab">
+            {{ option.label }}
+          </button>
+        </Tabs.Trigger>
+      </div>
+    </Tabs.List>
+  </Tabs.Root>
+</template>
+
+<script setup lang="ts">
+import { Tabs } from 'radix-vue/namespaced';
+
+defineProps<{
+  options: Item[];
+}>();
+
+const model = defineModel<Item['value']>();
+
+function handleUpdate(value: string): void {
+  model.value = value;
+}
+</script>
+
+<script lang="ts">
+interface Item {
+  value: string;
+  label: string;
+}
+
+export type { Item };
+</script>
+
+<style scoped>
+.root {
+  display: inline-flex;
+  flex-direction: column;
+}
+
+.list {
+  display: inline-flex;
+  gap: var(--spacing-7);
+}
+
+.tab {
+  padding: 0;
+  border: none;
+  outline: none;
+  background: transparent;
+  color: var(--color-text-secondary);
+  font-size: var(--font-size-s);
+  font-weight: var(--font-weight-heavy);
+  cursor: pointer;
+
+  &[data-state='active'] {
+    color: var(--color-text-primary);
+  }
+
+  &:hover {
+    color: var(--color-text-primary);
+  }
+}
+</style>
