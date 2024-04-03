6eefe7b8ac76dc97494cabd536318f56b31287c6
Timur Badretdinov <accounts@destiner.io>
2024-04-03 08:38:59 +0100

    Add general icon typing

---
+++ src/components/__common/icon/general/index.ts
@@ -0,0 +1,29 @@
+type Kind =
+  | 'arrow-left'
+  | 'arrow-right'
+  | 'arrow-top-right'
+  | 'bookmark'
+  | 'bookmark-filled'
+  | 'check'
+  | 'check-circled'
+  | 'chevron-down'
+  | 'chevron-left'
+  | 'chevron-right'
+  | 'chevron-up'
+  | 'circle'
+  | 'code'
+  | 'copy'
+  | 'cross'
+  | 'cross-circled'
+  | 'double-arrow-left'
+  | 'double-arrow-right'
+  | 'hex-string'
+  | 'input'
+  | 'json'
+  | 'loop'
+  | 'magnifying-glass'
+  | 'reload'
+  | 'text';
+
+// eslint-disable-next-line import/prefer-default-export
+export type { Kind };
