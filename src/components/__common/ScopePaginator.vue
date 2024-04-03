ff1f52a968a0b2ab2ba901a94a561f4c99fe692a
Timur Badretdinov <accounts@destiner.io>
2024-04-03 14:28:19 +0100

    Add ScopePaginator component

---
+++ src/components/__common/ScopePaginator.vue
@@ -0,0 +1,91 @@
+<template>
+  <Pagination.Root
+    :page="page"
+    :total="total"
+    :items-per-page="1"
+    :sibling-count="1"
+    show-edges
+    @update:page="handlePageUpdate"
+  >
+    <Pagination.List class="list">
+      <Pagination.Prev class="button button-arrow">
+        <ScopeIcon
+          class="icon"
+          kind="arrow-left"
+        />
+      </Pagination.Prev>
+      <Pagination.ListItem
+        class="button"
+        :value="page"
+      >
+        {{ page }}
+      </Pagination.ListItem>
+      <Pagination.Next class="button button-arrow">
+        <ScopeIcon
+          class="icon"
+          kind="arrow-right"
+        />
+      </Pagination.Next>
+    </Pagination.List>
+  </Pagination.Root>
+</template>
+
+<script setup lang="ts">
+import { Pagination } from 'radix-vue/namespaced';
+
+import ScopeIcon from './ScopeIcon.vue';
+
+defineProps<{
+  total: number;
+}>();
+
+const page = defineModel<number>({
+  required: true,
+});
+
+function handlePageUpdate(newValue: number): void {
+  page.value = newValue;
+}
+</script>
+
+<style scoped>
+.list {
+  display: flex;
+  gap: var(--spacing-4);
+  align-items: center;
+  justify-content: center;
+}
+
+.button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0;
+  border: none;
+  border-radius: var(--border-radius-s);
+  outline: none;
+  background: transparent;
+  color: var(--color-text-primary);
+  font-size: var(--font-size-m);
+}
+
+.button-arrow {
+  color: var(--color-text-secondary);
+
+  &:hover {
+    color: var(--color-text-primary);
+  }
+
+  &:disabled {
+    opacity: 0.5;
+    cursor: not-allowed;
+    pointer-events: none;
+  }
+}
+
+.icon {
+  width: 14px;
+  height: 14px;
+  cursor: pointer;
+}
+</style>
