ef044aa047987f3639dde6bc0c3c5ce370250b8d
Timur Badretdinov <accounts@destiner.io>
2024-04-04 09:38:41 +0100

    Add InputSearch component

---
+++ src/components/chain/InputSearch.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="wrapper">
+    <input
+      ref="inputEl"
+      placeholder="Address, transaction, or block"
+      :value
+      :disabled="isLoading"
+      @input="handleInput"
+      @keydown.enter="handleSubmit"
+    />
+    <div class="icon-wrapper">
+      <ScopeIcon
+        class="icon"
+        kind="arrow-right"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useFocus } from '@vueuse/core';
+import { ref } from 'vue';
+
+import ScopeIcon from '@/components/__common/ScopeIcon.vue';
+
+defineProps<{
+  isLoading: boolean;
+}>();
+
+const value = defineModel<string>({
+  required: true,
+});
+
+const emit = defineEmits<{
+  submit: [];
+}>();
+
+const inputEl = ref();
+useFocus(inputEl, { initialValue: true });
+
+function handleInput(event: Event): void {
+  const newValue = (event.target as HTMLInputElement).value;
+  value.value = newValue;
+}
+
+function handleSubmit(): void {
+  emit('submit');
+}
+</script>
+
+<style scoped>
+.wrapper {
+  position: relative;
+}
+
+input {
+  width: 100%;
+  padding: var(--spacing-4) var(--spacing-5);
+  transition: 0.25s ease-in-out;
+  border: 1px solid var(--color-border-secondary);
+  border-radius: var(--border-radius-s);
+  outline: none;
+  background: transparent;
+  box-shadow: inset 2px 2px 2px rgb(0 0 0 / 40%);
+  color: var(--color-text-primary);
+  font-size: var(--font-size-s);
+}
+
+input:disabled {
+  opacity: 0.6;
+}
+
+input:focus {
+  border-color: var(--color-border-primary);
+}
+
+input::placeholder {
+  color: var(--color-text-placeholder);
+}
+
+.icon-wrapper {
+  display: flex;
+  position: absolute;
+  top: 0;
+  right: 0;
+  align-items: center;
+  height: 100%;
+  padding: 0 8px;
+}
+
+.icon {
+  width: 15px;
+  height: 15px;
+  transition: 0.25s ease-in-out;
+  opacity: 0.8;
+  cursor: pointer;
+}
+
+.icon:hover {
+  opacity: 1;
+}
+</style>
