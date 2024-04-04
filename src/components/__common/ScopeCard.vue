1e42759d80ec1fdf3fef977c87767ec3f8d936a6
Timur Badretdinov <accounts@destiner.io>
2024-04-04 13:16:21 +0100

    Add ScopeCard component

---
+++ src/components/__common/ScopeCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="card">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.card {
+  padding: var(--spacing-7);
+  border: 1px solid var(--color-border-tertiary);
+  border-radius: var(--border-radius-m);
+}
+</style>
