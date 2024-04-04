91c78b0293e52aef5389f0082922e330b09167fb
Timur Badretdinov <accounts@destiner.io>
2024-04-04 13:17:50 +0100

    Add BlockStatus component

---
+++ src/components/block/BlockStatus.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="wrapper">
+    <div
+      class="status"
+      :class="{ success: status === 'executed' }"
+    >
+      <ScopeIcon
+        v-if="status === 'executed'"
+        :kind="'check-circled'"
+        class="icon"
+      />
+      {{ label }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+import ScopeIcon from '@/components/__common/ScopeIcon.vue';
+import type { BlockStatus } from '@/services/evm';
+
+const props = defineProps<{
+  status: BlockStatus;
+}>();
+
+const label = computed(() => {
+  const map: Record<BlockStatus, string> = {
+    executed: 'Executed',
+  };
+  return map[props.status];
+});
+</script>
+
+<style scoped>
+.wrapper {
+  display: flex;
+}
+
+.status {
+  display: flex;
+  gap: var(--spacing-4);
+  align-items: center;
+  padding: var(--spacing-3);
+  border: 1px solid var(--color-border-primary);
+  border-radius: var(--border-radius-m);
+  font-size: var(--font-size-m);
+}
+
+.status.success {
+  border-color: var(--color-success);
+  color: var(--color-success);
+}
+
+.icon {
+  width: 16px;
+  height: 16px;
+}
+</style>
