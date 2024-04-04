8e58f649e82553c8a174b83f3a3abfb6dae9cf31
Timur Badretdinov <accounts@destiner.io>
2024-04-04 13:17:39 +0100

    Add LinkAddress component

---
+++ src/components/__common/LinkAddress.vue
@@ -0,0 +1,30 @@
+<template>
+  <ScopeLinkInternal
+    :route="{
+      name: 'address',
+      address: address,
+    }"
+  >
+    <div class="content">
+      {{ address }}
+    </div>
+  </ScopeLinkInternal>
+</template>
+
+<script setup lang="ts">
+import { Address } from 'viem';
+
+import ScopeLinkInternal from './ScopeLinkInternal.vue';
+
+defineProps<{
+  address: Address;
+}>();
+</script>
+
+<style scoped>
+.content {
+  display: flex;
+  align-items: center;
+  gap: var(--spacing-2);
+}
+</style>
