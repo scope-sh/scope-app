ab516cad4edf8976fe7cf6b08d15e256d1c3e591
Timur Badretdinov <accounts@destiner.io>
2024-04-04 13:16:31 +0100

    Add ScopeLabelEmptyState component

---
+++ src/components/__common/ScopeLabelEmptyState.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>{{ value }}</div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  value: string;
+}>();
+</script>
+
+<style scoped>
+div {
+  margin: var(--spacing-6) 0;
+  color: var(--color-text-secondary);
+  font-size: var(--font-size-m);
+  text-align: center;
+}
+</style>
