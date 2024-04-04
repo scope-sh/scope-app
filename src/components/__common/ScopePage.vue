029d54d040ee59d41dca76b3f7326e2df5e7b26e
Timur Badretdinov <accounts@destiner.io>
2024-04-04 13:17:04 +0100

    Add ScopePage component

---
+++ src/components/__common/ScopePage.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="page">
+    <div class="header">
+      <ScopeTabs
+        v-model="section"
+        :options="sections"
+      />
+    </div>
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ScopeTabs from './ScopeTabs.vue';
+
+defineProps<{
+  sections: Section[];
+}>();
+
+const section = defineModel<Section['value']>('section', {
+  required: true,
+});
+</script>
+
+<script lang="ts">
+interface Section {
+  label: string;
+  value: string;
+}
+
+export type { Section };
+</script>
+
+<style scoped>
+.page {
+  display: flex;
+  gap: var(--spacing-7);
+  flex-direction: column;
+  align-items: center;
+  padding: var(--spacing-5) 8px;
+
+  @media (width > 768px) {
+    padding: var(--spacing-10) 96px 48px;
+  }
+}
+
+.header {
+  display: flex;
+  justify-content: end;
+  width: 100%;
+}
+
+main {
+  display: flex;
+  gap: var(--spacing-10);
+  flex-direction: column;
+  width: 100%;
+}
+</style>
