13c52c9bf5c0c018b4a0d32b8330a108e4cb0e06
Timur Badretdinov <accounts@destiner.io>
2024-04-08 19:18:22 +0100

    Add API endpoint to env vars

--- src/composables/useEnv.ts
+++ src/composables/useEnv.ts
@@ -1,11 +1,13 @@
 interface EnvironmentVariables {
   alchemyApiKey: string;
+  apiEndpoint: string;
 }
 
 function useEnv(): EnvironmentVariables {
   const env = (import.meta as ImportMeta).env;
   return {
     alchemyApiKey: env.VITE_ALCHEMY_API_KEY || '',
+    apiEndpoint: env.VITE_API_ENDPOINT || '',
   };
 }
 
