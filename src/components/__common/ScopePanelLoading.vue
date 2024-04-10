2a9c01eae50ed53804fa619c9d723da6119c2029
Timur Badretdinov <accounts@destiner.io>
2024-04-10 20:41:40 +0100

    Tweak colors in ScopePanelLoading

--- src/components/__common/ScopePanelLoading.vue
+++ src/components/__common/ScopePanelLoading.vue
@@ -28,27 +28,27 @@ defineProps<{
 }
 
 .icon {
-  --color: oklch(from var(--color-accent) l calc(c * 0.4) h / 100%);
+  --color-outline: oklch(from var(--color-accent) l calc(c * 0.2) h / 100%);
+  --color-glow: oklch(from var(--color-accent) l calc(c * 0.8) h / 100%);
 
   width: 32px;
   height: 32px;
   animation: glow 1.5s infinite alternate;
-  opacity: 0.75;
-  color: var(--color);
+  color: var(--color-outline);
 }
 
 @keyframes glow {
   0% {
-    color: var(--color);
-    filter: drop-shadow(0 0 5px oklch(from var(--color) l c h / 80%))
-      drop-shadow(0 0 10px oklch(from var(--color) l c h / 60%))
-      drop-shadow(0 0 15px oklch(from var(--color) l c h / 40%));
+    color: var(--color-outline);
+    filter: drop-shadow(0 0 5px oklch(from var(--color-glow) l c h / 80%))
+      drop-shadow(0 0 10px oklch(from var(--color-glow) l c h / 60%))
+      drop-shadow(0 0 15px oklch(from var(--color-glow) l c h / 40%));
   }
 
   100% {
-    color: oklch(from var(--color) l c h / 60%);
-    filter: drop-shadow(0 0 5px oklch(from var(--color) l c h / 60%))
-      drop-shadow(0 0 10px oklch(from var(--color) l c h / 40%));
+    color: oklch(from var(--color-outline) l c h / 60%);
+    filter: drop-shadow(0 0 5px oklch(from var(--color-glow) l c h / 60%))
+      drop-shadow(0 0 10px oklch(from var(--color-glow) l c h / 40%));
   }
 }
 </style>
