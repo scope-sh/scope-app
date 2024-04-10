cf582271aecacb0b4c8eae29fac8e2f1e5c6f58b
Timur Badretdinov <accounts@destiner.io>
2024-04-10 20:42:38 +0100

    Add FormLabel address component

---
+++ src/components/address/FormLabel.vue
@@ -0,0 +1,17 @@
+<template>
+  <label :for="id">{{ value }}</label>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  id?: string;
+  value: string;
+}>();
+</script>
+
+<style scoped>
+label {
+  font-family: var(--font-mono);
+  font-size: var(--font-size-s);
+}
+</style>
