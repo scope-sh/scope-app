58e4a709460947e29faa336d1134a99586f5b8a9
Timur Badretdinov <accounts@destiner.io>
2024-04-14 16:25:40 +0100

    Add UI store

---
+++ src/stores/ui.ts
@@ -0,0 +1,26 @@
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+
+import type { Toast } from '@/utils/ui';
+
+const store = defineStore('ui', () => {
+  const toast = ref<Toast | null>(null);
+  const isPaletteOpen = ref(false);
+
+  function setToast(newToast: Toast | null): void {
+    toast.value = newToast;
+  }
+
+  function setPaletteOpen(isOpen: boolean): void {
+    isPaletteOpen.value = isOpen;
+  }
+
+  return {
+    toast,
+    isPaletteOpen,
+    setToast,
+    setPaletteOpen,
+  };
+});
+
+export default store;
