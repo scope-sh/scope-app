e75fdef58f715038cf8eb4c5c9e070c2117082f5
Timur Badretdinov <accounts@destiner.io>
2024-04-15 21:07:21 +0100

    Update LensView component

--- src/components/address/LensView.vue
+++ src/components/address/LensView.vue
@@ -15,6 +15,16 @@
     v-else-if="labelTypeId === 'uniswap-v3-pool'"
     :address="address"
   />
+  <Erc7579ModuleLens
+    v-else-if="
+      labelTypeId === 'erc7579-module' || labelTypeId === 'rhinestone-v1-module'
+    "
+    :address="address"
+  />
+  <KernelV3AccountLens
+    v-else-if="labelTypeId === 'kernel-v3-account'"
+    :address="address"
+  />
 </template>
 
 <script setup lang="ts">
@@ -23,6 +33,8 @@ import { Address } from 'viem';
 import type { LabelId } from '@/services/api';
 
 import Erc20Lens from './lenses/Erc20Lens.vue';
+import Erc7579ModuleLens from './lenses/Erc7579ModuleLens.vue';
+import KernelV3AccountLens from './lenses/KernelV3AccountLens.vue';
 import SafeV1_4_1Lens from './lenses/SafeV1_4_1Lens.vue';
 import UniswapV2PoolLens from './lenses/UniswapV2PoolLens.vue';
 import UniswapV3PoolLens from './lenses/UniswapV3PoolLens.vue';
