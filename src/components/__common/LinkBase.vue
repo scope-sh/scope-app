b121d5a61504c605fc3503a8cd2fd8c64fcb3331
Timur Badretdinov <accounts@destiner.io>
2024-04-20 20:57:20 +0100

    Add LinkBase component

---
+++ src/components/__common/LinkBase.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="link">
+    <slot />
+    <LinkButtonCopy
+      v-if="type === 'normal'"
+      :value
+      class="button"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import LinkButtonCopy from './LinkButtonCopy.vue';
+import { Type } from './ScopeLinkInternal.vue';
+
+defineProps<{
+  value: string;
+  type: Type;
+}>();
+</script>
+
+<style scoped>
+.link {
+  display: inline-flex;
+  align-items: center;
+  gap: var(--spacing-4);
+
+  .button {
+    display: none;
+    cursor: pointer;
+  }
+
+  &:hover {
+    .button {
+      display: block;
+    }
+  }
+}
+</style>
