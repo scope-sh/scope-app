499d49700565388fa8901bc81d30f3925b0f4a53
Timur Badretdinov <accounts@destiner.io>
2024-04-21 11:36:00 +0100

    Add ScopeEmptyState component

---
+++ src/components/__common/ScopeEmptyState.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="root">
+    <ScopeLabelEmptyState :value="label" />
+    <div class="actions">
+      <slot name="actions" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ScopeLabelEmptyState from './ScopeLabelEmptyState.vue';
+
+defineProps<{
+  label: string;
+}>();
+</script>
+
+<style scoped>
+.root {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: var(--spacing-5);
+}
+
+.actions {
+  display: flex;
+  justify-content: center;
+  gap: var(--spacing-4);
+}
+</style>
