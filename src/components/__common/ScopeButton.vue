4501727f7e3b2b2ac17bee1cf8647437936212d2
Timur Badretdinov <accounts@destiner.io>
2024-04-21 11:35:50 +0100

    Add ScopeButton component

---
+++ src/components/__common/ScopeButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+button {
+  padding: var(--spacing-3) var(--spacing-5);
+  border: 1px solid var(--color-border-secondary);
+  border-radius: var(--border-radius-s);
+  background-color: transparent;
+  color: var(--color-text-primary);
+  font-size: var(--font-size-m);
+  cursor: pointer;
+
+  &:hover {
+    border-color: var(--color-border-primary);
+  }
+}
+</style>
