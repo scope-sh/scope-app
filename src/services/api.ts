eee2d90426006ab8ea1d6ffb5672d4c4a4aa0d1f
Timur Badretdinov <accounts@destiner.io>
2024-06-05 12:05:09 +0400

    Remove deprecated endpoints in API service

--- src/services/api.ts
+++ src/services/api.ts
@@ -134,38 +134,6 @@ class Service {
     this.chainId = chainId;
   }
 
-  public async getLabel(address: Address): Promise<LabelWithAddress | null> {
-    const params: Record<string, string> = {
-      chain: this.chainId.toString(),
-      address,
-    };
-    const url = new URL(`${apiEndpoint}/label/one`);
-    url.search = new URLSearchParams(params).toString();
-    const response = await fetch(url);
-    const label: LabelWithAddress | null = await response.json();
-    return label;
-  }
-
-  public async getLabels(
-    addresses: Address[],
-  ): Promise<Record<Address, Label>> {
-    const params: Record<string, string> = {
-      chain: this.chainId.toString(),
-    };
-    const body = JSON.stringify({ addresses });
-    const url = new URL(`${apiEndpoint}/label/many`);
-    url.search = new URLSearchParams(params).toString();
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body,
-    });
-    const labels: Record<Address, Label> = await response.json();
-    return labels;
-  }
-
   public async getAllAddressLabels(address: Address): Promise<Label[]> {
     const params: Record<string, string> = {
       chain: this.chainId.toString(),
