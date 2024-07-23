10ff23126eaf2a79adcfe1f933ee3b535a571821
Timur Badretdinov <accounts@destiner.io>
2024-07-24 00:03:46 +0100

    Add AbiForm components

---
+++ app/components/__common/abi-form/AbiFormInputBool.vue
@@ -0,0 +1,24 @@
+<template>
+  <input
+    :id="id"
+    :value="modelValue"
+    type="checkbox"
+    @update:modelValue="handleModelValueUpdate"
+  />
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+import { injectAbiFormInputContext } from './AbiFormInput.vue';
+
+const rootContext = injectAbiFormInputContext();
+const id = computed(() => rootContext.id.value);
+const modelValue = computed<boolean>(
+  () => rootContext.modelValue.value as boolean,
+);
+
+function handleModelValueUpdate(newValue: boolean): void {
+  rootContext.onModelValueUpdate(newValue);
+}
+</script>
