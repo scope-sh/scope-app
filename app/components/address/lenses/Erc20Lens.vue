bcdeea5d8a2af8c1dc41148db66e1fd31ac0a0a6
Timur Badretdinov <accounts@destiner.io>
2024-07-24 21:13:43 +0100

    Use LensQuery in Erc20Lens

--- app/components/address/lenses/Erc20Lens.vue
+++ app/components/address/lenses/Erc20Lens.vue
@@ -19,43 +19,29 @@
     <AttributeItem>
       <AttributeItemLabel :value="'Balance'" />
       <AttributeItemValue>
-        <LensForm
-          :abi-inputs="[
-            {
-              type: 'address',
-              name: 'owner',
-            },
-          ]"
-          :is-loading="isBalanceLoading"
-          @submit="fetchBalance"
-        >
-          <template #output>
-            <div>{{ balance }}</div>
-          </template>
-        </LensForm>
+        <LensQuery
+          :address="address"
+          :abi="ABI_ERC20"
+          :function-name="'balanceOf'"
+          :formatter="
+            (value) =>
+              decimals ? fromWei(value, decimals).toString() : value.toString()
+          "
+        />
       </AttributeItemValue>
     </AttributeItem>
     <AttributeItem>
       <AttributeItemLabel :value="'Allowance'" />
       <AttributeItemValue>
-        <LensForm
-          :abi-inputs="[
-            {
-              type: 'address',
-              name: 'owner',
-            },
-            {
-              type: 'address',
-              name: 'spender',
-            },
-          ]"
-          :is-loading="isAllowanceLoading"
-          @submit="fetchAllowance"
-        >
-          <template #output>
-            <div>{{ allowance }}</div>
-          </template>
-        </LensForm>
+        <LensQuery
+          :address="address"
+          :abi="ABI_ERC20"
+          :function-name="'allowance'"
+          :formatter="
+            (value) =>
+              decimals ? fromWei(value, decimals).toString() : value.toString()
+          "
+        />
       </AttributeItemValue>
     </AttributeItem>
   </LensBase>
@@ -66,7 +52,7 @@ import { type Address } from 'viem';
 import { ref, watch } from 'vue';
 
 import LensBase from './common/LensBase.vue';
-import LensForm from './common/LensForm.vue';
+import LensQuery from './common/LensQuery.vue';
 
 import ABI_ERC20 from '@/abi/erc20';
 import {
@@ -84,15 +70,11 @@ const props = defineProps<{
 }>();
 
 const isLoading = ref(true);
-const isBalanceLoading = ref(false);
-const isAllowanceLoading = ref(false);
 
 const symbol = ref<string | null>(null);
 const name = ref<string | null>(null);
 const decimals = ref<number | null>(null);
 const totalSupply = ref<number | null>(null);
-const balance = ref<string | null>(null);
-const allowance = ref<string | null>(null);
 
 watch(
   () => props.address,
@@ -141,43 +123,5 @@ async function fetch(): Promise<void> {
       : null;
 
   isLoading.value = false;
-}
-
-async function fetchBalance(inputs: unknown[]): Promise<void> {
-  const [owner] = inputs as [owner: string];
-  isBalanceLoading.value = true;
-  balance.value = null;
-  if (!client.value || !owner) return;
-
-  const result = await client.value.readContract({
-    address: props.address as Address,
-    abi: ABI_ERC20,
-    functionName: 'balanceOf',
-    args: [owner as Address],
-  });
-
-  isBalanceLoading.value = false;
-  balance.value = decimals.value
-    ? fromWei(result, decimals.value).toString()
-    : result.toString();
-}
-
-async function fetchAllowance(inputs: unknown[]): Promise<void> {
-  const [owner, spender] = inputs as [owner: string, spender: string];
-  isAllowanceLoading.value = true;
-  allowance.value = null;
-  if (!client.value || !owner || !spender) return;
-
-  const result = await client.value.readContract({
-    address: props.address as Address,
-    abi: ABI_ERC20,
-    functionName: 'allowance',
-    args: [owner as Address, spender as Address],
-  });
-
-  isAllowanceLoading.value = false;
-  allowance.value = decimals.value
-    ? fromWei(result, decimals.value).toString()
-    : result.toString();
 }
 </script>
