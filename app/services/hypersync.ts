d27bf88ed3337a22dbff89abcd4aba1ea731e0c3
Timur Badretdinov <accounts@destiner.io>
2024-07-27 19:56:52 +0100

    Rewrite Hypersync service to use API endpoints

--- app/services/hypersync.ts
+++ app/services/hypersync.ts
@@ -5,92 +5,14 @@ import type { Chain } from '@/utils/chains.js';
 
 type Sort = 'asc' | 'desc';
 
-type BlockFieldSelection = 'number' | 'timestamp';
-type TransactionFieldSelection =
-  | 'block_number'
-  | 'transaction_index'
-  | 'hash'
-  | 'from'
-  | 'to'
-  | 'input'
-  | 'value'
-  | 'gas_price'
-  | 'status';
-type LogFieldSelection =
-  | 'log_index'
-  | 'transaction_hash'
-  | 'block_number'
-  | 'address'
-  | 'data'
-  | 'topic0'
-  | 'topic1'
-  | 'topic2'
-  | 'topic3';
-
-interface Query {
-  from_block?: number;
-  to_block?: number;
-  logs?: {
-    address?: Address[];
-    topics?: Hex[][];
-  }[];
-  transactions?: {
-    from?: Address[];
-    to?: Address[];
-  }[];
-  max_num_transactions?: number;
-  max_num_logs?: number;
-  sort?: Sort;
-  field_selection: {
-    block?: BlockFieldSelection[];
-    transaction?: TransactionFieldSelection[];
-    log?: LogFieldSelection[];
-  };
-}
-
-interface QueryResponse {
-  data: {
-    blocks?: QueryBlock[];
-    transactions?: QueryTransaction[];
-    logs?: QueryLog[];
-  }[];
-  next_block?: number;
-  prev_block?: number;
-  archive_height: number;
-}
-
-interface QueryBlock {
-  number: number;
-  timestamp: Hex;
-}
-
-interface QueryTransaction {
-  block_number: number;
-  from: Address;
-  gas_price: Hex;
-  hash: Hex;
-  input: Hex;
-  to: Hex | null;
-  transaction_index: number;
-  value: Hex;
-  status: 1;
-}
-
-interface QueryLog {
-  log_index: number;
-  transaction_hash: Hex;
-  block_number: number;
-  address: Address;
-  data: Hex;
-  topic0: Hex;
-  topic1: Hex;
-  topic2: Hex;
-  topic3: null;
-}
-
 interface Pagination {
   cursor: number | null;
   height: number | null;
+}
+
+interface AddressTransactionsResponse {
+  transactions: TransactionResponse[];
+  pagination: Pagination;
 }
 
 interface AddressTransactions {
@@ -101,6 +23,19 @@ interface AddressTransactions {
 interface AddressLogs {
   logs: Log[];
   pagination: Pagination;
+}
+
+interface TransactionResponse {
+  blockNumber: number;
+  blockTimestamp: number;
+  from: Address;
+  gasPrice: string;
+  hash: Hex;
+  input: Hex;
+  to: Address | null;
+  transactionIndex: number;
+  value: string;
+  status: number;
 }
 
 interface Transaction {
@@ -133,12 +68,12 @@ class Service {
   constructor(chain: Chain) {
     this.chain = chain;
     this.client = ky.create({
-      prefixUrl: this.#getEndpoint(chain),
+      prefixUrl: 'http://localhost:5173/api',
     });
   }
 
   getSort(): Sort {
-    return 'asc';
+    return 'desc';
   }
 
   async getAddressTransactions(
@@ -147,99 +82,24 @@ class Service {
     limit: number,
     sort: Sort,
   ): Promise<AddressTransactions> {
-    const transactions: Transaction[] = [];
-    let cursor = startCursor;
-    let height: number = Infinity;
-    while (
-      transactions.length < limit &&
-      this.#withInBounds(sort, height, cursor)
-    ) {
-      const response = await this.#getAddressTransactionsPartial(
+    const response = await this.client.get('transactions', {
+      searchParams: {
+        chain: this.chain,
         address,
-        cursor,
+        cursor: startCursor ? startCursor : 0,
         limit,
         sort,
-      );
-      const data = response.data;
-      const newTransactions = data
-        .map((dataPage) => {
-          const pageTransactions = dataPage.transactions || [];
-          return pageTransactions.map((transaction) => {
-            const transactionBlock = dataPage.blocks?.find(
-              (block) => block.number === transaction.block_number,
-            );
-            const timestamp = transactionBlock?.timestamp || '0x';
-            return {
-              blockNumber: transaction.block_number,
-              blockTimestamp: 1000 * parseInt(timestamp),
-              from: transaction.from,
-              gasPrice: BigInt(transaction.gas_price),
-              hash: transaction.hash,
-              input: transaction.input,
-              to: transaction.to,
-              transactionIndex: transaction.transaction_index,
-              value: BigInt(transaction.value),
-              status: transaction.status,
-            };
-          });
-        })
-        .flat();
-      transactions.push(...newTransactions);
-      cursor =
-        sort === 'asc'
-          ? response.next_block || Infinity
-          : response.prev_block || 0;
-      height = response.archive_height;
-    }
-    return {
-      transactions,
-      pagination: {
-        cursor,
-        height,
       },
-    };
-  }
-
-  async #getAddressTransactionsPartial(
-    address: Address,
-    cursor: number | null,
-    limit: number,
-    sort: Sort,
-  ): Promise<QueryResponse> {
-    const query: Query = {
-      from_block: sort === 'asc' ? cursor || 0 : undefined,
-      to_block: sort === 'desc' ? cursor || undefined : undefined,
-      transactions: [
-        {
-          from: [address],
-        },
-        {
-          to: [address],
-        },
-      ],
-      max_num_transactions: limit,
-      sort,
-      field_selection: {
-        block: ['number', 'timestamp'],
-        transaction: [
-          'block_number',
-          'transaction_index',
-          'hash',
-          'from',
-          'to',
-          'input',
-          'value',
-          'gas_price',
-          'status',
-        ],
-      },
-    };
-
-    const response = await this.client.post('query', {
-      json: query,
     });
-    const json = (await response.json()) as QueryResponse;
-    return json;
+    const transactions = await response.json<AddressTransactionsResponse>();
+    return {
+      transactions: transactions.transactions.map((transaction) => ({
+        ...transaction,
+        gasPrice: BigInt(transaction.gasPrice),
+        value: BigInt(transaction.value),
+      })),
+      pagination: transactions.pagination,
+    };
   }
 
   async getAddressLogs(
@@ -248,107 +108,16 @@ class Service {
     limit: number,
     sort: Sort,
   ): Promise<AddressLogs> {
-    const logs: Log[] = [];
-    let cursor = startCursor;
-    let height: number = Infinity;
-    while (logs.length < limit && this.#withInBounds(sort, height, cursor)) {
-      const response = await this.#getAddressLogsPartial(
+    const response = await this.client.get('logs', {
+      searchParams: {
+        chain: this.chain,
         address,
-        cursor,
+        cursor: startCursor ? startCursor : 0,
         limit,
         sort,
-      );
-      const data = response.data;
-      const newLogs = data
-        .map((dataPage) => {
-          const pageLogs = dataPage.logs || [];
-          return pageLogs.map((log) => {
-            const logBlock = dataPage.blocks?.find(
-              (block) => block.number === log.block_number,
-            );
-            const timestamp = logBlock?.timestamp || '0x';
-            return {
-              blockNumber: log.block_number,
-              blockTimestamp: 1000 * parseInt(timestamp),
-              logIndex: log.log_index,
-              transactionHash: log.transaction_hash,
-              address: log.address,
-              data: log.data,
-              topics: [log.topic0, log.topic1, log.topic2, log.topic3].filter(
-                (topic) => topic !== null,
-              ),
-            };
-          });
-        })
-        .flat();
-      logs.push(...newLogs);
-      cursor =
-        sort === 'asc'
-          ? response.next_block || Infinity
-          : response.prev_block || 0;
-      height = response.archive_height;
-    }
-    return {
-      logs,
-      pagination: {
-        cursor,
-        height,
       },
-    };
-  }
-
-  async #getAddressLogsPartial(
-    address: Address,
-    cursor: number | null,
-    limit: number,
-    sort: Sort,
-  ): Promise<QueryResponse> {
-    const query: Query = {
-      from_block: sort === 'asc' ? cursor || 0 : undefined,
-      to_block: sort === 'desc' ? cursor || undefined : undefined,
-      logs: [
-        {
-          address: [address],
-        },
-      ],
-      max_num_logs: limit,
-      sort,
-      field_selection: {
-        block: ['number', 'timestamp'],
-        log: [
-          'log_index',
-          'transaction_hash',
-          'block_number',
-          'address',
-          'data',
-          'topic0',
-          'topic1',
-          'topic2',
-          'topic3',
-        ],
-      },
-    };
-
-    const response = await this.client.post('query', {
-      json: query,
     });
-    const json = (await response.json()) as QueryResponse;
-    return json;
-  }
-
-  #withInBounds(sort: Sort, height: number, cursor: number | null): boolean {
-    if (cursor === null) {
-      return true;
-    }
-    if (sort === 'asc') {
-      return cursor < height;
-    } else {
-      return cursor > 0;
-    }
-  }
-
-  #getEndpoint(chain: Chain): string {
-    return `https://${chain}.hypersync.xyz`;
+    return response.json<AddressLogs>();
   }
 }
 
