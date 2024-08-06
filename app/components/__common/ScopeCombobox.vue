a6338f19ac362d99ba2ed25e20e3d32b3e389c22
Timur Badretdinov <accounts@destiner.io>
2024-08-06 14:36:56 +0100

    Add ScopeCombobox component

---
+++ app/components/__common/ScopeCombobox.vue
@@ -0,0 +1,221 @@
+<template>
+  <div class="view">
+    <Combobox.Root
+      v-model="model"
+      class="root"
+      :filter-function="filterOptions"
+    >
+      <Combobox.Anchor class="anchor">
+        <Combobox.Input
+          class="input"
+          placeholder="Search functions…"
+        />
+        <ScopeIcon
+          v-if="model"
+          kind="cross"
+          class="icon-close"
+          @click="model = undefined"
+        />
+        <Combobox.Trigger>
+          <ScopeIcon
+            kind="chevron-down"
+            class="icon"
+          />
+        </Combobox.Trigger>
+      </Combobox.Anchor>
+
+      <Combobox.Content class="content">
+        <Combobox.Viewport class="viewport">
+          <Combobox.Empty class="empty" />
+
+          <Combobox.Group
+            v-for="(group, groupIndex) of options"
+            :key="groupIndex"
+          >
+            <Combobox.Label class="label"> {{ group.label }} </Combobox.Label>
+
+            <Combobox.Item
+              v-for="(option, optionIndex) in group.options"
+              :key="optionIndex"
+              class="item"
+              :value="option"
+            >
+              <Combobox.ItemIndicator class="indicator">
+                <ScopeIcon
+                  kind="check"
+                  class="icon"
+                />
+              </Combobox.ItemIndicator>
+              <span>
+                {{ option }}
+              </span>
+            </Combobox.Item>
+            <Combobox.Separator
+              v-if="groupIndex !== options.length - 1"
+              class="separator"
+            />
+          </Combobox.Group>
+        </Combobox.Viewport>
+      </Combobox.Content>
+    </Combobox.Root>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Combobox } from 'radix-vue/namespaced';
+
+import ScopeIcon from '@/components/__common/ScopeIcon.vue';
+
+defineProps<{
+  options: OptionGroup[];
+}>();
+
+const model = defineModel<Option | undefined>();
+
+// function filterOptions(options: Option[], query: string): Option[] {
+//   return options.filter((option) =>
+//     option.label.toLowerCase().includes(query.toLowerCase()),
+//   );
+// }
+
+function filterOptions(options: Option[], query: string): string[] {
+  return options.filter((option) =>
+    option.toLowerCase().includes(query.toLowerCase()),
+  );
+}
+</script>
+
+<script lang="ts">
+interface OptionGroup {
+  label: string;
+  options: Option[];
+}
+
+// interface Option {
+//   value: string;
+//   label: string;
+// }
+
+type Option = string;
+
+export type { OptionGroup, Option };
+</script>
+
+<style scoped>
+/* reset */
+button,
+input {
+  all: unset;
+}
+
+.view {
+  width: 200px;
+}
+
+.root {
+  position: relative;
+}
+
+.anchor {
+  display: inline-flex;
+  align-items: center;
+  width: 100%;
+  padding: var(--spacing-4);
+  border-radius: var(--border-radius-s);
+  background-color: var(--color-background-secondary);
+  color: var(--color-text-primary);
+  font-size: var(--font-size-m);
+  gap: var(--spacing-3);
+}
+
+.anchor:hover {
+  background-color: var(--color-background-tertiary);
+}
+
+.input {
+  flex: 1;
+  min-width: 100px;
+  background: transparent;
+  color: var(--color-text-primary);
+}
+
+.input[data-placeholder] {
+  color: var(--color-text-placeholder);
+}
+
+.icon-close {
+  width: 14px;
+  height: 14px;
+}
+
+.icon {
+  width: 16px;
+  height: 16px;
+}
+
+.content {
+  position: absolute;
+  z-index: 1;
+  min-width: 160px;
+  max-height: 60vh;
+  margin-top: var(--spacing-4);
+  overflow: hidden;
+  border: 1px solid var(--color-border-tertiary);
+  border-radius: var(--border-radius-m);
+  background: var(--color-background-primary);
+}
+
+.viewport {
+  padding: var(--spacing-3);
+}
+
+.empty {
+  padding: var(--spacing-4);
+  color: var(--color-text-secondary);
+  font-size: var(--font-size-s);
+  text-align: center;
+}
+
+.item {
+  display: flex;
+  position: relative;
+  align-items: center;
+  padding: var(--spacing-3) var(--spacing-10);
+  border-radius: var(--border-radius-s);
+  color: var(--color-text-primary);
+  font-size: var(--font-size-m);
+  cursor: pointer;
+}
+
+.item[data-disabled] {
+  opacity: 0.4;
+  pointer-events: none;
+}
+
+.item[data-highlighted] {
+  outline: none;
+  background-color: var(--color-background-secondary);
+  color: var(--color-text-primary);
+}
+
+.label {
+  padding: var(--spacing-3) var(--spacing-10);
+  color: var(--color-text-secondary);
+  font-size: var(--font-size-s);
+}
+
+.separator {
+  height: 1px;
+  margin: var(--spacing-2) 0;
+  background-color: var(--color-border-tertiary);
+}
+
+.indicator {
+  display: inline-flex;
+  position: absolute;
+  left: 0;
+  align-items: center;
+  justify-content: center;
+  padding: var(--spacing-4);
+}
+</style>
