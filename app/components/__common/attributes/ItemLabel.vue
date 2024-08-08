ada9a0a7be88f8bcf52acbce257db41e07db955a
Timur Badretdinov <accounts@destiner.io>
2024-08-08 19:04:50 +0100

    Add optional "note" prop to attribute ItemLabel

--- app/components/__common/attributes/ItemLabel.vue
+++ app/components/__common/attributes/ItemLabel.vue
@@ -1,23 +1,51 @@
 <template>
-  <div class="label">{{ value }}</div>
+  <div class="label">
+    <ScopeTooltip v-if="note">
+      <template #trigger>
+        <div class="value">
+          {{ value }}
+        </div>
+      </template>
+      <template #default>
+        <div class="note">{{ note }}</div>
+      </template>
+    </ScopeTooltip>
+    <div
+      v-else
+      class="label value"
+    >
+      {{ value }}
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+import ScopeTooltip from '@/components/__common/ScopeTooltip.vue';
+
 defineProps<{
   value: string;
+  note?: string;
 }>();
 </script>
 
 <style scoped>
-.label {
+.value {
   color: var(--color-text-secondary);
   font-size: var(--font-size-s);
+  text-align: left;
   text-transform: uppercase;
+}
+
+.note {
+  max-width: 240px;
 }
 
 @media (width >= 768px) {
   .label {
     width: 224px;
+  }
+
+  .value {
     font-size: var(--font-size-m);
   }
 }
