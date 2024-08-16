ce9298219a6b6c1300094b8ca2ae70796e40262d
Timur Badretdinov <accounts@destiner.io>
2024-08-16 16:53:57 +0100

    Add hovered link to UI store

--- app/stores/ui.ts
+++ app/stores/ui.ts
@@ -1,11 +1,12 @@
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
-import type { Toast } from '@/utils/ui.js';
+import type { Toast, Link } from '@/utils/ui.js';
 
 const store = defineStore('ui', () => {
   const toast = ref<Toast | null>(null);
   const isPaletteOpen = ref(false);
+  const linkHover = ref<Link | null>(null);
 
   function setToast(newToast: Toast | null): void {
     toast.value = newToast;
@@ -15,11 +16,17 @@ const store = defineStore('ui', () => {
     isPaletteOpen.value = isOpen;
   }
 
+  function setLinkHover(link: Link | null): void {
+    linkHover.value = link;
+  }
+
   return {
     toast,
     isPaletteOpen,
+    linkHover,
     setToast,
     setPaletteOpen,
+    setLinkHover,
   };
 });
 
--- app/utils/ui.ts
+++ app/utils/ui.ts
@@ -1,3 +1,5 @@
+import type { Address, Hex } from 'viem';
+
 const TOAST_DURATION = 5000;
 
 interface Toast {
@@ -5,5 +7,27 @@ interface Toast {
   message: string;
 }
 
+interface BlockLink {
+  type: 'block';
+  value: bigint;
+}
+
+interface TransactionHashLink {
+  type: 'transaction';
+  value: Hex;
+}
+
+interface UserOpHashLink {
+  type: 'userop';
+  value: Hex;
+}
+
+interface AddressLink {
+  type: 'address';
+  value: Address;
+}
+
+type Link = BlockLink | TransactionHashLink | UserOpHashLink | AddressLink;
+
 export { TOAST_DURATION };
-export type { Toast };
+export type { Toast, Link };
