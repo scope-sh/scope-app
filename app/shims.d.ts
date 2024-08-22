e5600c20d0b12f72e1f53425e7d025d36a30b926
Timur Badretdinov <accounts@destiner.io>
2024-08-22 13:52:59 +0100

    Add Quicknode env vars to shims

--- app/shims.d.ts
+++ app/shims.d.ts
@@ -8,6 +8,8 @@ declare module '*.vue' {
 interface ImportMeta {
   env: {
     VITE_ALCHEMY_API_KEY?: string;
+    VITE_QUICKNODE_APP_NAME?: string;
+    VITE_QUICKNODE_APP_KEY?: string;
     VITE_APP_BASE_URL?: string;
     VITE_API_ENDPOINT?: string;
     VITE_INDEXER_ENDPOINT?: string;
