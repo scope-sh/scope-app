d16871351a2b7bca310b82360283c6763cec053d
Timur Badretdinov <accounts@destiner.io>
2024-08-22 13:53:08 +0100

    Add Quicknode env vars to useEnv

--- app/composables/useEnv.ts
+++ app/composables/useEnv.ts
@@ -1,5 +1,7 @@
 interface EnvironmentVariables {
   alchemyApiKey: string;
+  quicknodeAppName: string;
+  quicknodeAppKey: string;
   apiEndpoint: string;
   appBaseUrl: string;
   indexerEndpoint: string;
@@ -9,6 +11,8 @@ function useEnv(): EnvironmentVariables {
   const env = (import.meta as ImportMeta).env;
   return {
     alchemyApiKey: env.VITE_ALCHEMY_API_KEY || '',
+    quicknodeAppName: env.VITE_QUICKNODE_APP_NAME || '',
+    quicknodeAppKey: env.VITE_QUICKNODE_APP_KEY || '',
     apiEndpoint: env.VITE_API_ENDPOINT || '',
     appBaseUrl: env.VITE_APP_BASE_URL || '',
     indexerEndpoint: env.VITE_INDEXER_ENDPOINT || '',
