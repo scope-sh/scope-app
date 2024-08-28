c434eca86423b76e070fe58b4f5abb18ed751b0c
Timur Badretdinov <accounts@destiner.io>
2024-08-28 23:45:51 +0100

    Add CardDeployment address component

---
+++ app/components/address/CardDeployment.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="card">
+    <div class="item">
+      Deployed by
+      <LinkAddress :address="deployment.deployer" />
+    </div>
+    <div class="item">
+      in tx
+      <LinkTransaction :hash="deployment.transactionHash">
+        {{ deployment.transactionHash.slice(0, 8) }}…{{
+          deployment.transactionHash.slice(-8)
+        }}
+      </LinkTransaction>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import LinkAddress from '@/components/__common/LinkAddress.vue';
+import LinkTransaction from '@/components/__common/LinkTransaction.vue';
+import type { Deployment } from '@/services/api';
+
+defineProps<{
+  deployment: Deployment;
+}>();
+</script>
+
+<style scoped>
+.card {
+  display: flex;
+  gap: var(--spacing-2) var(--spacing-3);
+  flex-wrap: wrap;
+  padding: var(--spacing-4);
+  border: 1px solid var(--color-border-tertiary);
+  border-radius: var(--border-radius-m);
+  font-size: var(--font-size-s);
+
+  .item {
+    display: flex;
+    gap: var(--spacing-3);
+    align-items: center;
+  }
+}
+</style>
