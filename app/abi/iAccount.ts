ed387765f841102fe3f07b9d7fc7fe608bee7faf
Timur Badretdinov <accounts@destiner.io>
2024-09-10 17:03:38 +0100

    Add IAccount ABI

---
+++ app/abi/iAccount.ts
@@ -0,0 +1,309 @@
+const abi = [
+  {
+    inputs: [
+      {
+        internalType: 'bytes',
+        name: 'initCode',
+        type: 'bytes',
+      },
+    ],
+    name: 'createSender',
+    outputs: [
+      {
+        internalType: 'address',
+        name: 'sender',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        components: [
+          { internalType: 'address', name: 'sender', type: 'address' },
+          { internalType: 'uint256', name: 'nonce', type: 'uint256' },
+          { internalType: 'bytes', name: 'initCode', type: 'bytes' },
+          { internalType: 'bytes', name: 'callData', type: 'bytes' },
+          {
+            internalType: 'uint256',
+            name: 'callGasLimit',
+            type: 'uint256',
+          },
+          {
+            internalType: 'uint256',
+            name: 'verificationGasLimit',
+            type: 'uint256',
+          },
+          {
+            internalType: 'uint256',
+            name: 'preVerificationGas',
+            type: 'uint256',
+          },
+          {
+            internalType: 'uint256',
+            name: 'maxFeePerGas',
+            type: 'uint256',
+          },
+          {
+            internalType: 'uint256',
+            name: 'maxPriorityFeePerGas',
+            type: 'uint256',
+          },
+          {
+            internalType: 'bytes',
+            name: 'paymasterAndData',
+            type: 'bytes',
+          },
+          { internalType: 'bytes', name: 'signature', type: 'bytes' },
+        ],
+        internalType: 'struct UserOperation',
+        name: 'userOp',
+        type: 'tuple',
+      },
+      {
+        internalType: 'bytes32',
+        name: 'userOpHash',
+        type: 'bytes32',
+      },
+      {
+        internalType: 'uint256',
+        name: 'missingAccountFunds',
+        type: 'uint256',
+      },
+    ],
+    name: 'validateUserOp',
+    outputs: [
+      { internalType: 'uint256', name: 'validationData', type: 'uint256' },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        components: [
+          {
+            internalType: 'address',
+            name: 'sender',
+            type: 'address',
+          },
+          {
+            internalType: 'uint256',
+            name: 'nonce',
+            type: 'uint256',
+          },
+          {
+            internalType: 'bytes',
+            name: 'initCode',
+            type: 'bytes',
+          },
+          {
+            internalType: 'bytes',
+            name: 'callData',
+            type: 'bytes',
+          },
+          {
+            internalType: 'bytes32',
+            name: 'accountGasLimits',
+            type: 'bytes32',
+          },
+          {
+            internalType: 'uint256',
+            name: 'preVerificationGas',
+            type: 'uint256',
+          },
+          {
+            internalType: 'bytes32',
+            name: 'gasFees',
+            type: 'bytes32',
+          },
+          {
+            internalType: 'bytes',
+            name: 'paymasterAndData',
+            type: 'bytes',
+          },
+          {
+            internalType: 'bytes',
+            name: 'signature',
+            type: 'bytes',
+          },
+        ],
+        internalType: 'struct PackedUserOperation',
+        name: 'userOp',
+        type: 'tuple',
+      },
+      {
+        internalType: 'bytes32',
+        name: 'userOpHash',
+        type: 'bytes32',
+      },
+      {
+        internalType: 'uint256',
+        name: 'missingAccountFunds',
+        type: 'uint256',
+      },
+    ],
+    name: 'validateUserOp',
+    outputs: [
+      { internalType: 'uint256', name: 'validationData', type: 'uint256' },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        components: [
+          { internalType: 'address', name: 'sender', type: 'address' },
+          { internalType: 'uint256', name: 'nonce', type: 'uint256' },
+          { internalType: 'bytes', name: 'initCode', type: 'bytes' },
+          { internalType: 'bytes', name: 'callData', type: 'bytes' },
+          {
+            internalType: 'uint256',
+            name: 'callGasLimit',
+            type: 'uint256',
+          },
+          {
+            internalType: 'uint256',
+            name: 'verificationGasLimit',
+            type: 'uint256',
+          },
+          {
+            internalType: 'uint256',
+            name: 'preVerificationGas',
+            type: 'uint256',
+          },
+          {
+            internalType: 'uint256',
+            name: 'maxFeePerGas',
+            type: 'uint256',
+          },
+          {
+            internalType: 'uint256',
+            name: 'maxPriorityFeePerGas',
+            type: 'uint256',
+          },
+          {
+            internalType: 'bytes',
+            name: 'paymasterAndData',
+            type: 'bytes',
+          },
+          { internalType: 'bytes', name: 'signature', type: 'bytes' },
+        ],
+        internalType: 'struct UserOperation',
+        name: 'userOp',
+        type: 'tuple',
+      },
+      {
+        internalType: 'bytes32',
+        name: 'userOpHash',
+        type: 'bytes32',
+      },
+      {
+        internalType: 'uint256',
+        name: 'maxCost',
+        type: 'uint256',
+      },
+    ],
+    name: 'validatePaymasterUserOp',
+    outputs: [
+      {
+        internalType: 'bytes',
+        name: 'context',
+        type: 'bytes',
+      },
+      {
+        internalType: 'uint256',
+        name: 'validationData',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        components: [
+          {
+            internalType: 'address',
+            name: 'sender',
+            type: 'address',
+          },
+          {
+            internalType: 'uint256',
+            name: 'nonce',
+            type: 'uint256',
+          },
+          {
+            internalType: 'bytes',
+            name: 'initCode',
+            type: 'bytes',
+          },
+          {
+            internalType: 'bytes',
+            name: 'callData',
+            type: 'bytes',
+          },
+          {
+            internalType: 'bytes32',
+            name: 'accountGasLimits',
+            type: 'bytes32',
+          },
+          {
+            internalType: 'uint256',
+            name: 'preVerificationGas',
+            type: 'uint256',
+          },
+          {
+            internalType: 'bytes32',
+            name: 'gasFees',
+            type: 'bytes32',
+          },
+          {
+            internalType: 'bytes',
+            name: 'paymasterAndData',
+            type: 'bytes',
+          },
+          {
+            internalType: 'bytes',
+            name: 'signature',
+            type: 'bytes',
+          },
+        ],
+        internalType: 'struct PackedUserOperation',
+        name: 'userOp',
+        type: 'tuple',
+      },
+      {
+        internalType: 'bytes32',
+        name: 'userOpHash',
+        type: 'bytes32',
+      },
+      {
+        internalType: 'uint256',
+        name: 'maxCost',
+        type: 'uint256',
+      },
+    ],
+    name: 'validatePaymasterUserOp',
+    outputs: [
+      {
+        internalType: 'bytes',
+        name: 'context',
+        type: 'bytes',
+      },
+      {
+        internalType: 'uint256',
+        name: 'validationData',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+] as const;
+
+export default abi;
